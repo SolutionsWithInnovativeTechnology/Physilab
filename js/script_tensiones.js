37255757c062b62f510d5ca0f53be97d8912d224
Saúl <sauln559@gmail.com>
2024-11-23 20:47:54 -0500

    pascal, caidalibre

--- js/script_tensiones.js
+++ js/script_tensiones.js
@@ -155,6 +155,12 @@ function updateDiagram() {
     ctx.fillText(`${p} N`, 135, 205);
 }
 
+/**
+ * Calcula las tensiones desconocidas y el peso dadas las tensiones y
+ * ángulos ingresados.
+ *
+ * @returns {void}
+ */
 function calculate() {
     const tensiones = [];
     const inputsTension = document.querySelectorAll('.tension-input');
